8c977e20ef51fa085f15a344462f539ba7af377b
Rafael jupy <rafasud.rafael@Gmail.com>
2026-07-04 17:24:57 -0300

    finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random generator = new Random();
+        int number = generator.Next(1, 101);
+
+        int number2 = -1;
+        
+
+        while (number != number2)
+        {
+            Console.WriteLine("What is your guess number?");
+            string guess = Console.ReadLine();
+            number2 = int.Parse(guess);
+                          
+            if (number > number2)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < number2)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number == number2)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            
+        }
     }
 }
