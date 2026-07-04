c9317e2969fa04a725bffbbfc6416c78cf1a0efa
Rafael jupy <rafasud.rafael@Gmail.com>
2026-07-04 16:10:45 -0300

    Exercise 2 finished

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int information = int.Parse(grade);
+
+        if (information >= 0 && information <= 100)
+        {
+            if (information >= 90)
+            {
+                Console.WriteLine("Your Letter grade is: A ");
+            }
+            else if (information >= 80)
+            {
+                Console.WriteLine("Your Letter grade is: B ");   
+            }
+            else if (information >= 70)
+            {
+                Console.WriteLine("Your Letter grade is: C ");   
+            }
+            else if (information >= 60)
+            {
+                Console.WriteLine("Your Letter grade is: D ");  
+            }
+            else if (information < 60)
+            {
+                Console.WriteLine("Your Letter grade is: F ");  
+            }
+        }
+        else
+        {
+            Console.WriteLine("Type a valid grade value"); 
+        }
     }
 }
